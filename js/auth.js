a8fe8828a772ce6eff019833c553002bf1f1a21e
Katherine024z <00045724@uca.edu.sv>
2025-10-29 14:16:59 -0600

    feat: Eliminar duplicados

--- js/auth.js
+++
@@ -1,9 +0,0 @@
-const formLogin = document.getElementById("formLogin");
-
-if (formLogin) {
-  formLogin.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const usuario = document.getElementById("usuario").value;
-    alert(`Bienvenido ${usuario}! (Inicio de sesión simulado)`);
-  });
-}
