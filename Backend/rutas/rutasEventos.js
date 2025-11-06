fc00481d0c95ec8f18babe6ee27176b1e2183af3
W4samGG7 <00097524@uca.edu.sv>
2025-11-06 16:46:11 -0600

    feat:Agregar ruta para obtener eventos filtrados en backend

--- Backend/rutas/rutasEventos.js
+++ Backend/rutas/rutasEventos.js
@@ -25,4 +25,25 @@ router.get('/eventos', async (req, res) => {
     }
 });
 
+//ruta para obtener eventos filtrados
+router.get("/eventos/categoria/:nombreCategoria", async (req, res) => {
+  try {
+    const { nombreCategoria } = req.params;
+
+    const consulta = `
+    SELECT e.nombre, e.fecha, c.categoria FROM Evento E
+    JOIN Categoria C ON E.id_categoria = C.id
+    WHERE C.categoria = @categoria
+    ORDER BY e.fecha ASC`;
+      
+    const pool = req.db;
+    const resultado = await pool
+      .request()
+      .input("categoria", sql.VarChar, nombreCategoria) 
+      .query(consulta);
+    res.json(resultado.recordset);
+  } catch (err) {
+    console.error("Error en GET /eventos/categoria:", err.message);
+    res.status(500).json({ mensaje: "Error al filtrar eventos por categoria" });
+  }});
 export default router;
