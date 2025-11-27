ea6684d91cc9c440061cea124012da0b9fce2ef1
W4samGG7 <00097524@uca.edu.sv>
2025-11-27 07:21:49 -0600

    feat: implementar logica en "be" para inscribirse a un evento

---
+++ Backend/controladores/inscribirseEventos.js
@@ -0,0 +1,68 @@
+import { sql } from "../data/dbConfig.js";
+
+const InscribirseEvento = async (req, res) => {
+  try{
+  const { id_evento } = req.body;
+
+  const {carnet} = req.user;
+
+  if(!id_evento) {
+    return res.status(400).json({mensaje: "ID de evento no proporcionado"})
+  }
+
+  const pool = req.db;
+
+  const consultaEvento = "SELECT nombre, cupos, privado FROM EVENTO WHERE id = @id_evento";
+
+  const resultadoEvento = await pool.request()
+    .input("id_evento", sql.Int, id_evento)
+    .query(consultaEvento);
+
+  if(resultadoEvento.recordset.length === 0){
+    return res.status(404).json({mensaje: "El evento no existe"});
+  }
+
+  const evento = resultadoEvento.recordset[0];
+  const cuposMaximos = evento.cupos;
+
+  const consultaUsuarioInscrito = "SELECT id FROM Inscripcion WHERE id_evento = @id_evento AND id_usuario = @carnet";
+  const resultadoUsuarioInscrito = await pool.request()
+    .input("id_evento", sql.Int, id_evento)
+    .input("canet", sql.VarChar, carnet)
+    .query(consultaUsuarioInscrito);
+
+  if (resultadoUsuarioInscrito.recordset.length > 0){
+    return res.status(409).json({mensaje: "Ya estas suscrito a este evento"});
+  }
+
+  if(cuposMaximos > 0){
+    const consultaCuposTotal = "SELECT COUNT(*) as total FROM Inscripcion WHERE id_evento = @id_evento";
+    const resultadoCuposTotal = await pool.request()
+      .input("id_evento", sql.Int, id_evento)
+      .query(consultaCuposTotal);
+
+      const inscritosActuales = resultadoCuposTotal.recordset[0].total;
+
+      if(inscritosActuales >= cuposMaximos){
+        return res.status(409).json({mensaje: "Los cupos estan llenos para este evento"});
+      }
+  }
+
+  const insertarInscripcion = `
+  INSERT INTO Inscripcion (id_evento, id_usuario)
+  VALUES (@id_evento, @carnet)
+  `;
+
+  await pool.request()
+    .input("id_evento", sql.Int, id_evento)
+    .input("carnet", sql.VarChar, carnet)
+    .query(insertarInscripcion);
+
+  res.status(201).json({mensaje: `inscripcion exitosa al evento: ${evento.nombre}`})
+  }catch (err){
+    console.error("Error en inscripcion: ", err.message);
+    res.status(500).json({mensaje: "Error en el servidor al realizar la inscripcion"})
+  }
+};
+
+export default InscribirseEvento;
--- Backend/enrutador/rutas.js
+++ Backend/enrutador/rutas.js
@@ -8,6 +8,7 @@ import buscarEventos from '../controladores/busquedaRuta.js';
 import IniciarSesion from "../controladores/iniciarSesion.js";
 import registrarse  from "../controladores/registrarUsuario.js";
 import ProponerEvento from "../controladores/proponerEvento.js";
+import InscribirseEvento from "../controladores/inscribirseEventos.js";
 
 const rutas = Router();
 
@@ -18,5 +19,7 @@ rutas.get('/eventos/destacados',getEventosDestacados);
 rutas.post('/login', IniciarSesion);
 rutas.post('/registro',registrarse);
 rutas.post('/eventos/proponer', verifyToken, ProponerEvento);
+rutas.post('/eventos/inscribirse', verifyToken, InscribirseEvento);
+
 
 export default rutas;
